b39e264154ec3b770e001d72dd711abd02a4d7e0
nthuyouwei <145022311+nthuyouwei@users.noreply.github.com>
2023-10-15 23:14:58 +0800

    Update tb.v

--- lab3/final_ed/tb.v
+++ lab3/final_ed/tb.v
@@ -351,7 +351,7 @@ module bram11
     reg [31:0] Temp_D;
     always @(posedge CLK) begin
         if(EN) begin
-	        if(WE[0]) RAM[A>>2][7:0] <= Di[7:0];
+            if(WE[0]) RAM[A>>2][7:0] <= Di[7:0];
             if(WE[1]) RAM[A>>2][15:8] <= Di[15:8];
             if(WE[2]) RAM[A>>2][23:16] <= Di[23:16];
             if(WE[3]) RAM[A>>2][31:24] <= Di[31:24];
