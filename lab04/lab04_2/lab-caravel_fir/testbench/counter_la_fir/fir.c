49646d3e6b9479c352c66e81454209823055dd0e
nthuyouwei <145022311+nthuyouwei@users.noreply.github.com>
2023-12-07 18:30:18 +0800

    Update fir.c

--- lab04/lab04_2/lab-caravel_fir/testbench/counter_la_fir/fir.c
+++ lab04/lab04_2/lab-caravel_fir/testbench/counter_la_fir/fir.c
@@ -19,18 +19,20 @@ int* __attribute__ ( ( section ( ".mprjram" ) ) ) fir(){
 	uint8_t  register i=0;
 	reg_fir_control = 1; //set ap_start, bit[0] = 1
 
-
+        reg_fir_x = 0;
 	
-	while(i<N){
+	while(i<N-1){
 	       
 	        //while((reg_fir_control >> 4) & 1 !=1); // external signal x[n] ready, wait until bit[4] = 1
 	      //while((reg_fir_control & 0x10 )==0);
-		reg_fir_x = i;
+		//reg_fir_x = i;
 		
 		//while((reg_fir_control >> 5) & 1!=1); // external signal y[n] ready, wait until bit[5] = 1
 		outputsignal[i] = reg_fir_y;
 		i=i+1;
+		reg_fir_x = i;
 	}
+	outputsignal[N-1] = reg_fir_y;
 	while((reg_fir_control >> 1) & 1 != 1); // read ap_done, bit[1] = 1
 
 	return &outputsignal[63];
